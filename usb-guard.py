a49470c5366003cd16170b9572e61264e9366e24
Jānis Jansons <admin@janhouse.lv>
2012-12-31 02:29:56 +0100

    Done more work on UI.

--- usb-guard.py
+++ usb-guard.py
@@ -3,7 +3,11 @@
 # Copyright 2012 Janis Jansons (janis.jansons@janhouse.lv)
 #
 
+import gi
+# make sure you use gtk+-2.0
+gi.require_version('Gtk', '3.0')
 import sys, os
+import struct
 from gi.repository import GObject, Polkit, GLib, Notify, GUdev, Gio, Gtk
 
 class UsbGuard:
@@ -54,6 +58,11 @@ class UsbGuard:
         self.builder = Gtk.Builder()
         self.builder.add_from_file("main.ui")
 
+        self.window = self.builder.get_object("window1")
+        
+        
+        self.style = self.window.get_style_context()
+
         # pack the table into the scrolled window
         self.container = self.builder.get_object("dev_container")#.add_with_viewport(table)
 
@@ -67,15 +76,14 @@ class UsbGuard:
 
         self.sort_devices()
 
-        for device in self.devices:
-            self.print_device(device)
+        #for device in self.devices:
+        #    self.print_device(device)
 
 
         ###############
-
-
-        # Make the main window visible
-        self.builder.get_object("window1").show()
+        
+        # Make the main window visible      
+        self.window.show()
         # Start the main loop
         self.mainloop.run ()
         
@@ -192,7 +200,6 @@ class UsbGuard:
             print "   device property %s: %s"  % (device_key, device.get_property(device_key))
 
 
-
     def on_uevent (self, client, action, device):
         print ("action " + action + " on device " + device.get_sysfs_path())
         #if device.get_subsystem() == "usb":
@@ -200,29 +207,79 @@ class UsbGuard:
             self.on_add(device)
             #if action == "remove":
 
+
     def rec_print(self, devtree, dev, lev):
         if lev > 0:
             
-            print "Level: "+str(lev)+"; Device: "+dev
-            print devtree[dev]
-            print "\n\n\n"
+            #print "Level: "+str(lev)+"; Device: "+dev
+            #print devtree[dev]
+            #print "\n\n\n"
             device=devtree[dev]['device']
             
             
-            label = "%s %s" % ( #\nPath: %s
+            color=color_to_string(self.style.lookup_color('info_fg_color')[1])
+            #color="gray"
+            
+            label_text = "<span foreground='%s'><i>%s</i></span>, %s" % ( #\nPath: %s
+                color,
                 self.device_vendor_string(device), 
                 self.device_model_string(device), 
                 #device.get_sysfs_path().rsplit('/', 1)[0] + '/'
                 )
             #label=device_vendor_string(device)+", "+device_model_string(device)
             #label="wololo"
+            label_text=label_text.replace('\x20', ' ')
 
-            button = Gtk.CheckButton(label)
-            #button.set_halign(Gtk.Align.START)
-            button.set_margin_left(25*(lev-1))
-            #button.halign(Gtk.GTK_ALIGN_START)
-            self.container_box.pack_start(button, False, False, 0)
-            button.show()
+            if devtree[dev]['hub'] == False:
+                
+                box = Gtk.Box(spacing=5, orientation=Gtk.Orientation.HORIZONTAL)
+                self.container_box.pack_start(box, False, False, 0)
+                
+                box.set_margin_left(( 25*(lev-1) )+25)
+                
+                box.show()
+                
+                
+                label = Gtk.Label()
+                label.set_markup(label_text.decode('string-escape'))
+                #label.set_justify(Gtk.Justification.LEFT)
+                #button.add(label)
+                
+                
+                #button = Gtk.CheckButton()
+                #button.set_halign(Gtk.Align.START)
+                #button.set_margin_left(25*(lev-1))
+                #button.halign(Gtk.GTK_ALIGN_START)
+                box.pack_start(label, False, False, 0)
+                label.show()
+                
+                
+                button = Gtk.Button("Enable device")
+                box.pack_start(button, False, False, 0)
+                
+                button.show()
+                
+                always = Gtk.CheckButton("Enable always")
+                #button.set_halign(Gtk.Align.START)
+                #always.set_margin_left(25*(lev-1))
+                #button.halign(Gtk.GTK_ALIGN_START)
+                box.pack_start(always, False, False, 0)
+                always.show()
+
+            else:
+                
+                button = Gtk.CheckButton()
+                #button.set_halign(Gtk.Align.START)
+                button.set_margin_left(25*(lev-1))
+                #button.halign(Gtk.GTK_ALIGN_START)
+                self.container_box.pack_start(button, False, False, 0)
+                button.show()
+                
+                label = Gtk.Label()
+                label.set_markup(label_text.decode('string-escape'))
+                #label.set_justify(Gtk.Justification.LEFT)
+                button.add(label)
+                label.show()
         
         
         for dp in self.devtree[dev]['children']:
@@ -271,17 +328,10 @@ class UsbGuard:
             devpath=device.get_sysfs_path()
             dev = devpath.rsplit('/', 1)[1].rsplit(':', 1)[0] + '/'
             
-            #print "Devs:"
-            #print device.get_driver()
-            #print device.get_sysfs_path()
-            #print dev
             if dev not in self.devtree:
                 self.devtree[dev]={'name': dev, 'has_par': True,'children':[], 'hub':False, 'device':device}
                 par = devpath.rsplit('/', 2)[1] + '/'
-                print "VAI IR IEKSAA::"
-                print dev
-                print self.devtree[par]['children']
-                print "\n\n"
+
                 if dev not in self.devtree[par]['children']:
                     self.devtree[par]['children'].append(dev)
             
@@ -291,7 +341,13 @@ class UsbGuard:
                 continue
             self.rec_print(self.devtree, dev, 0)
 
-        print self.devtree
+        #print self.devtree
+
+def color_to_string(gdkcolor):
+    color = gdkcolor.to_string()
+    rgb = color[4:len(color)-1].split(',')
+    rgb2 = (int(num) for num in rgb)
+    return '#'+"".join(map(chr, rgb2)).encode('hex')
 
 def main():
     u=UsbGuard()
